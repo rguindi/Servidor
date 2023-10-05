a1791629091b99857fdd24fcf45b0fd6533a9936
rguindi <raul_fv@hotmail.com>
2023-10-05 21:28:18 +0000

    Piramidaconseguida

--- Tema3/piramide.php
+++ Tema3/piramide.php
@@ -1,15 +1,23 @@
 <?php
-    $altura =7;
+    $altura =45;
     $espacios = $altura -1;
     $asteriscos = 1;
 
    
     for ($i=0; $i < $altura ; $i++) { 
        
-       echo " ";
-         for ($ç=0; $ç < $altura ; $ç++) { 
-             echo "*";
-         }
+        global $espacios;
+       for ($it=0; $it < $espacios ; $it++) { 
+        echo "&nbsp";
+       }
+       $espacios--;
+       
+
+       for ($ite=0; $ite <$asteriscos ; $ite++) { 
+            echo "*";
+       }
+       echo "<br>";
+       $asteriscos+=2;
        
     }
 
