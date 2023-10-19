f2d3cc7a5efa770608b49aa179a3fe54a4035965
rguindi <raul_fv@hotmail.com>
2023-10-19 18:41:53 +0000

    TrabajoClase, formularios

---
+++ Tema3/formulario.php
@@ -0,0 +1,79 @@
+<?php
+ include ("./validaciones.php")
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        .error{
+            color: red;
+        }
+    </style>
+</head>
+<?php
+
+$errores = array ();
+if (enviado()){
+ if (textVacio("nombre")) $errores ['nombre'] = "Nombre vacio.";
+ if (textVacio("apellido")) $errores ['apellido'] = "Apellido vacio.";
+}
+?>
+
+<body>
+    <form action="" method="post" name="formulario1" enctype="multipart/form-data">
+    <label for="nombre">Nombre:<input type="text" name="nombre" id="nombre" 
+    
+    value= <?php
+     recuerda ('nombre');
+    ?>
+    
+    ></label>
+    <p class="error"><?php errores ($errores,'nombre');?></p>
+
+    <label for="apellido">Apellido: <input type="text" name="apellido" id="apellido" value=
+    <?php
+     recuerda ('apellido');
+    ?>></label>
+    <p class="error"><?php errores ($errores,'apellido');?></p>
+    <label for="hombre">Hombre<input type="radio" name="genero" id="hombre" value="hombre"></label>
+    <label for="hombre">Mujer<input type="radio" name="genero" id="mujer" value="mujer"></label>
+
+    <br>
+    <p>Aficciones</p>
+    <label for="ch1">Montar a Caballo<input type="checkbox" name="aficcion[]" id="ch1"  value="jinete"></label>
+    <label for="ch2">Jugar al Futbol<input type="checkbox" name="aficcion[]" id="ch2" value="futbolista"></label>
+    <label for="ch3">Nadar<input type="checkbox" name="aficcion[]" id="ch3" value="nadador"></label>
+    <br>
+
+    <label for="fecha_n">Fecha Nacimiento <input type="datetime-local" name="fecha_nac" id="fecha_nac"></label>
+    <br>
+
+    <p>Equipos Baloncesto</p>
+
+    <select name="equipos" id="">
+    <option value="0">Seleccione una opcion</option>
+    <option value="lakers">Lakers</option>
+    <option value="celtics">Celtics</option>
+    <option value="chicago">Chicago</option>
+
+    </select>
+
+    <br>  <br>
+
+    <input type="file" name="fichero" id="fichero">  
+
+    <input type="hidden" name="usuariovip" value="123">
+
+
+    <input type="submit" value="Enviar" name="Enviar">
+    <input type="reset" value="Borrar" name="Borrar">
+    
+
+</form>
+</body>
+</html>
---
+++ Tema3/procesa.php
@@ -0,0 +1,13 @@
+<?php
+
+//echo "hola " . $_GET ["nombre"];
+
+echo "<pre>";
+
+print_r ($_REQUEST);
+echo "</pre>";
+
+echo "<pre>";
+print_r ($_FILES);
+echo "</pre>";
+?>
