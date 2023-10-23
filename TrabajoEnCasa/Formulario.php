694c1eaed3a6016b49a63738b9ee961f28e35bc1
rguindi <raul_fv@hotmail.com>
2023-10-23 08:07:36 +0200

    trabajo en casa

---
+++ TrabajoEnCasa/Formulario.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="formulario.css">
+    <title>Formulario en Casa</title>
+</head>
+<body>
+    <header>
+        <img src="../img/perfil.jpg" alt="Perfil">
+        <div>Raul Ferrero Vicente</div>
+        <div>Esto es un Formulario</div>
+    </header>
+
+    <main>
+        <nav>
+            <div>Opcion1</div>
+            <div>Opcion2</div>
+            <div>Opcion3</div>
+            <div>Opcion4</div>
+            
+        </nav>
+    </main>
+
+    <footer>
+        <div>Parte1</div>
+        <div>Parte2</div>
+        <div>Parte 3</div>
+    </footer>
+    
+</body>
+</html>
