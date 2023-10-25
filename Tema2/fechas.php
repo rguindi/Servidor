b6b0b0cbccf59a5c8649cf5a7806004550d0c25b
rguindi <raul_fv@hotmail.com>
2023-10-25 13:23:52 +0200

    fechas corregidas

--- Tema2/fechas.php
+++ Tema2/fechas.php
@@ -8,12 +8,14 @@ echo date_default_timezone_get();
 echo "<p>Cambiada</p>";
 date_default_timezone_set ("Europe/Madrid");
 echo date_default_timezone_get();
+echo "<br>";
 
 echo date ("r");
+echo "<br>";
 echo date ("d/m/y H:i:s");
 
 echo "<p>Fechas cumnpleaños</p>";
-$cumplea = strtotime ("24/21/1982");
+$cumplea = strtotime ("09/24/1982");
 echo $cumplea;
 echo "<p>" .date("d/m/y", $cumplea) . "</p>";
 
@@ -24,5 +26,7 @@ echo "<p>" .date("d/m/y", $hoy) . "</p>";
 
 $edad = $hoy-$cumplea;
  echo $edad;
+ echo "<br>";
+ echo "Tengo " . floor(($hoy - $cumplea) / (60 * 60 * 24 * 365)) . " años";
 
 ?>
