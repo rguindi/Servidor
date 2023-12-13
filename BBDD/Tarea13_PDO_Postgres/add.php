d781ca80b0c0ef087db947b904217e9b7692b42c
rguindi <raul_fv@hotmail.com>
2023-12-13 09:01:54 +0000

    si

---
+++ BBDD/Tarea13_PDO_Postgres/add.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./estilos.css">
+    <title>Añadir Registro</title>
+</head>
+
+<body>
+    <?php
+    include("./funcionesFormulario.php");
+    include("./funcionesBD.php");
+    $errores = array();
+    
+    //Si ha ido todo bienb
+    if (enviado() && validaFormulario($errores)) {
+        addRegistro();
+        header("Location: ./index.php");
+
+
+    } else {
+
+        ?>
+
+
+
+        <main>
+            <!-- FORMULARIO -->
+
+            <form action="" method="get">
+
+                <label for="nombre">Nombre <input type="text" name="nombre" id="nombre" value=<?php recuerda('nombre') ?>></label> <br>
+                <?php printerror($errores, 'nombre'); 
+                      printerror($errores, 'validarNombre');   ?>
+                <label for="posicion">Posición <input type="text" name="posicion" id="posicion" value=<?php recuerda('posicion') ?>></label> <br>
+                <?php printerror($errores, 'posicion'); 
+                      printerror($errores, 'validarPosicion'); ?>
+                <label for="DNI">DNI <input type="text" name="DNI" id="DNI" value=<?php recuerda('DNI') ?>></label> <br>
+                <?php printerror($errores, 'DNI');
+                      printerror($errores, 'DNIMAL'); ?>
+                <label for="fecha">Fecha de Nacimiento<input type="date" name="fecha" id="fecha" placeholder="DD/MM/AAAA" value=<?php recuerda('fecha') ?>></label> <br>
+                <?php printerror($errores, 'fecha'); 
+                      printerror($errores, 'formatoFecha');
+                      ?>
+                <label for="sueldo">Sueldo <input type="text" name="sueldo" id="sueldo" value=<?php recuerda('sueldo') ?>></label> <br>
+                <?php printerror($errores, 'sueldo'); 
+                      printerror($errores, 'formatoSueldo');
+                      ?>
+                <label for="dorsal">Dorsal <input type="number" name="dorsal" id="dorsal" value=<?php recuerda('dorsal') ?>></label> <br>
+                <?php printerror($errores, 'dorsal'); 
+                      printerror($errores, 'formatoDorsal'); ?>
+
+                <input type="submit" value="Enviar" name="Enviar">
+                <input type="submit" value="Borrar" name="Borrar">
+
+
+            </form>
+
+            <?php
+    } //Cerramos el else
+    ?>
+    </main>
+
+
+</body>
+
+</html>
---
+++ BBDD/Tarea13_PDO_Postgres/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="#" type="image/x-icon">
+    <link rel="stylesheet" href="./estilos.css">
+</head>
+<body>
+    <?php
+    require("./funcionesBD.php");
+      if (isset($_REQUEST['crear'])) {
+       cargarScript();
+      }
+      if (isset($_REQUEST['add'])) {
+        header('Location: ./add.php');
+      }
+      
+      
+      ?>
+    <br><br>
+    
+    <form action="" method="get">
+       <input name = 'leer' type="submit" value="Mostrar toda la tabla">  </form>   <br>
+       <form action="" method="get">
+       <label for="busca">Buscar por DNI <input type="text" name="busca" id="busca"></label> 
+       <input name = 'botonBusca' type="submit" value="Buscar">   <br>
+       <label for="add">Añadir  <input name = 'add' type="submit" value="+"></label><br>
+       <?php if (isset ($_REQUEST['botonBusca']) && empty ($_REQUEST['busca']))  echo "<p class = error>Introduzca un DNI o parte de él para buscar</p>"; ?>
+      </form>
+      <br>
+      
+      <?php 
+      if (!empty($_REQUEST['eliminar'])) {
+        eliminarRegistro($_REQUEST['DNI']);
+        leerTabla();
+      }
+ if (isset($_REQUEST['leer'])) {
+    leerTabla(); 
+ }
+
+  if (isset($_REQUEST['botonBusca'])) {
+      buscarPorDNI($_REQUEST['busca']); 
+  }
+ ?>
+
+
+
+    
+</body>
+</html>
