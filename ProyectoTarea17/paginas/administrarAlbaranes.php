2967438f99dd34af4927f8854c28765b246cded5
rguindi <raul_fv@hotmail.com>
2023-12-26 23:09:38 +0000

    jjk

--- ProyectoTarea17/paginas/administrarAlbaranes.php
+++ ProyectoTarea17/paginas/administrarAlbaranes.php
@@ -55,7 +55,7 @@ if (!isset($_SESSION['usuario'])) {
                     </div>
                 </div>
                 <div class="row mt-3 border-bottom border-black  ">
-                    <div class="col-2 text-center  ">
+                    <div class="col-1 text-center  ">
                         <h5>Id</h5>
                     </div>
                     <div class="col-2 text-start  ">
@@ -70,7 +70,7 @@ if (!isset($_SESSION['usuario'])) {
                     <div class="col-2 text-center  ">
                         <h5>Usuario</h5>
                     </div>
-                    <div class="col-2 text-center  ">
+                    <div class="col-3 text-center  ">
                     <a href="./nuevoAlbaran.php"> <button class="btn btn-warning mb-2">Nuevo Albarán</button></a>
                     </div>
 
@@ -78,9 +78,10 @@ if (!isset($_SESSION['usuario'])) {
                 <?php
                 foreach ($albaranes as $key => $value) {
                     $producto = getProducto($value['cod_producto']);
+                    if (isModerador($_SESSION['usuario']['user'])){
                    echo'
                    <div class="row mt-3 border-bottom border-black">
-                    <div class="col-2 text-center  ">
+                    <div class="col-1 text-center  ">
                         <h5>'.$value['Id'].'</h5>
                     </div>
                     <div class="col-2 text-start  ">
@@ -96,14 +97,37 @@ if (!isset($_SESSION['usuario'])) {
                     </div>
                     <div class="col-2 text-center  ">
                         <h5>'.$value['usuario'].'</h5>
+                    </div>
                     </div>';
+                }else if (isAdmin($_SESSION['usuario']['user'])){
+                    echo'
+                    <div class="row mt-3 border-bottom border-black">
+                     <div class="col-1 text-center  ">
+                         <h5>'.$value['Id'].'</h5>
+                     </div>
+                     <div class="col-2 text-start  ">
+                         <h5>'.$producto['titulo'].'</h5>
+                     </div>
+                     <div class="col-2 text-center  ">
+                         <h5>';
+                         echo $value['cantidad'];
+                         echo'</h5>
+                     </div>
+                     <div class="col-2 text-center  ">
+                         <h5>'.$value['fecha'].'</h5>
+                     </div>
+                     <div class="col-2 text-center  ">
+                         <h5>'.$value['usuario'].'</h5>
+                     </div>
+                     <div class="col-3 text-center  ">
+                          <button class="btn btn-secondary mb-2">Modificar</button>
+                          <button class="btn btn-danger mb-2">Eliminar</button>
+                      </div>
+                     </div>';
+                }
 
-                //    echo '<div class="col-2 text-center  ">
-                //         <button class="btn btn-warning mb-2">Factura</button>
-                //     </div>
 
-                // </div>
-                //    ';
+              
                 }
                ?>
 
--- ProyectoTarea17/paginas/administrarPedidos.php
+++ ProyectoTarea17/paginas/administrarPedidos.php
@@ -55,7 +55,7 @@ if (!isset($_SESSION['usuario'])) {
                     </div>
                 </div>
                 <div class="row mt-3 border-bottom border-black  ">
-                    <div class="col-2 text-center  ">
+                    <div class="col-1 text-center  ">
                         <h5>Id</h5>
                     </div>
                     <div class="col-2 text-center  ">
@@ -64,13 +64,13 @@ if (!isset($_SESSION['usuario'])) {
                     <div class="col-2 text-center  ">
                         <h5>Producto</h5>
                     </div>
-                    <div class="col-2 text-center  ">
+                    <div class="col-1 text-center  ">
                         <h5>Cantidad</h5>
                     </div>
                     <div class="col-2 text-center  ">
                         <h5>Precio</h5>
                     </div>
-                    <div class="col-2 text-center  ">
+                    <div class="col-4 text-center  ">
                         <h5>Factura</h5>
                     </div>
 
@@ -78,6 +78,7 @@ if (!isset($_SESSION['usuario'])) {
                 <?php
                 foreach ($pedidos as $key => $value) {
                     $producto= getProducto($value['cod_producto']);
+                    if (isModerador($_SESSION['usuario']['user'])){
                    echo'
                    <div class="row mt-3 border-bottom border-black">
                     <div class="col-2 text-center  ">
@@ -103,6 +104,35 @@ if (!isset($_SESSION['usuario'])) {
 
                 </div>
                    ';
+                    }else if (isAdmin($_SESSION['usuario']['user'])){
+                        echo'
+                   <div class="row mt-3 border-bottom border-black">
+                    <div class="col-1 text-center  ">
+                        <h5>'.$value['Id'].'</h5>
+                    </div>
+                    <div class="col-2 text-center  ">
+                        <h5>'.$value['fecha'].'</h5>
+                    </div>
+                    <div class="col-2 text-center  ">
+                        <h5>';
+                        echo $producto['titulo'];
+                        echo '</h5>
+                    </div>
+                    <div class="col-1 text-center  ">
+                        <h5>'.$value['cantidad'].'</h5>
+                    </div>
+                    <div class="col-2 text-center  ">
+                        <h5>'.$value['total'].'</h5>
+                    </div>
+                    <div class="col-4 text-center  ">
+                        <button class="btn btn-warning mb-2">Factura</button>
+                        <button class="btn btn-secondary mb-2">Modificar</button>
+                        <button class="btn btn-danger mb-2">Eliminar</button>
+                    </div>
+
+                </div>
+                   ';
+                    }
                 }
                ?>
 
